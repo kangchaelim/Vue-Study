0d162182a25a730d5530ee3dd1b9a8a0f0e60de7
CHAELIM <kcl75597639@gmail.com>
2024-06-19 21:45:36 +0900

    문자열 DataBinding

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DataBinding from '../views/DataBinding.vue'
+
 
 const routes = [
   {
@@ -10,10 +12,15 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // route level code-splitting 라우트 레벨에서 코드를 분할하는 방법입니다.
+    // this generates a separate chunk (about.[hash].js) for this route 이 라우트에 대한 chunk 파일이 분리되어 생성됩니다.
+    // which is lazy-loaded when the route is visited. 이 라우트에 방문했을 때 lazy-load(지연로드) 됩니다.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
   }
 ]
 
